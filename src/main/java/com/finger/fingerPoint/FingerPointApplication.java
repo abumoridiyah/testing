df47ef6482e763a4c4b6690c35bd519a800fe7b8
YusuffSaliu <yusuffsaliu@comsoftltd.com>
2024-11-07 12:54:41 +0100

    updating first

---
+++ src/main/java/com/finger/fingerPoint/FingerPointApplication.java
@@ -0,0 +1,13 @@
+package com.finger.fingerPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingerPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finger/fingerPoint/FingerPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finger.fingerPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingerPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
